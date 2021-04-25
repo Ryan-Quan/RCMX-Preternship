282135d1362dd900d4bb4527bd550b6f6d6c7656
Ryan Quan <64490852+Ryan-Quan@users.noreply.github.com>
2021-04-25 13:43:16 -0400

    Create FuturesOPENINTEREST.py

---
+++ FuturesOPENINTEREST.py
@@ -0,0 +1,28 @@
+class FuturesOpenInterest(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2013, 12, 20) 
+        self.SetEndDate(2014, 2, 20) 
+        self.SetCash(1000000) 
+        self.gold = self.AddFuture(Futures.Metals.Gold) 
+        self.gold.SetFilter(0, 90)
+      
+    def OnData(self, slice):
+       
+        # Looping over each available futures chain from slice.FutureChains data
+        for chain in slice.FutureChains:
+            
+            #1. Filter to choose popular contracts with OpenInterest > 1000
+            print(chain)
+            self.popularContracts = [contract for contract in chain.Value if contract.Volume > 1000]
+                
+            
+            #2. If the length of contracts in this chain is zero, continue to the next chain
+            if len(self.popularContracts) == 0:
+                continue
+            
+            #3. Sort our contracts by open interest in descending order and save to sortedByOIContracts
+            sortedByOIContracts = sorted(self.popularContracts, key=lambda k : k.OpenInterest, reverse=True)
+            
+            #4. Save the contract with the highest open interest to self.liquidContract
+            self.liquidContract = sortedByOIContracts[0]
